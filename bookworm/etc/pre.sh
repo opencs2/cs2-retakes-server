8aa0e99e25b3587c7ffea51d68b956bf787233ed
krbtgt <144264613+goldenkirbi@users.noreply.github.com>
2025-01-09 22:43:08 +0000

    update plugins

--- bookworm/etc/pre.sh
+++ bookworm/etc/pre.sh
@@ -9,6 +9,6 @@ wget -qO- https://mms.alliedmods.net/mmsdrop/2.0/mmsource-2.0.0-git1314-linux.ta
     && wget -qO- https://github.com/B3none/cs2-retakes/releases/download/20.0.16/cs2-retakes-20.0.16.zip | bsdtar -xvf- -C "${STEAMAPPDIR}"/game/csgo/addons/counterstrikesharp/plugins/ \
     && wget -qO- https://github.com/B3none/cs2-retakes/releases/download/20.0.16/cs2-retakes-shared-20.0.16.zip | bsdtar -xvf- -C "${STEAMAPPDIR}"/game/csgo/ \
     && wget -qO- https://github.com/B3none/cs2-instadefuse/releases/download/2.0.0/cs2-instadefuse-2.0.0.zip  | bsdtar -xvf- -C "${STEAMAPPDIR}"/game/csgo/addons/counterstrikesharp/plugins/ \
-    && wget -qO- https://github.com/opencs2/cs2-agones/releases/download/0.0.3/cs2-agones.tar.gz | bsdtar -xvf- -C "${STEAMAPPDIR}"/game/csgo/addons/counterstrikesharp/plugins/ \
+    && wget -qO- https://github.com/opencs2/cs2-agones/releases/download/0.0.5/cs2-agones.tar.gz | bsdtar -xvf- -C "${STEAMAPPDIR}"/game/csgo/addons/counterstrikesharp/plugins/ \
     && sed -i '/Game_LowViolence/a \\t\t\tGame\tcsgo/addons/metamod' ${STEAMAPPDIR}/game/csgo/gameinfo.gi
 
